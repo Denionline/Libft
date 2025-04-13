5d2b6c0e108f0e20cd4eb98fcbd44fdf5703972f
Denionline <danielximenes164@gmail.com>
2025-04-13 17:30:23 +0100

    fix: Fixed the condition, to verify just if the list is true on the fist "if"

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 13:53:36 by dximenes          #+#    #+#             */
-/*   Updated: 2025/04/12 17:44:36 by dximenes         ###   ########.fr       */
+/*   Updated: 2025/04/13 17:27:39 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,14 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
 	t_list	*node;
 
-	if (lst && del)
+	if (lst)
 	{
 		node = (*lst);
 		while (node != NULL)
 		{
 			node = (*lst)->next;
-			del((*lst)->content);
+			if (del)
+				del((*lst)->content);
 			free(*lst);
 			(*lst) = node;
 		}
