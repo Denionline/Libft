99571862eb1ca209ec8ff7428c7989fba28a1ed0
Denionline <danielximenes164@gmail.com>
2025-04-22 20:51:13 +0100

    feat: New main.c on gitignore

--- main.c
+++ main.c
@@ -6,19 +6,21 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 20:31:28 by dximenes          #+#    #+#             */
-/*   Updated: 2025/04/20 20:33:34 by dximenes         ###   ########.fr       */
+/*   Updated: 2025/04/22 20:46:13 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-
 
 #include "libft.h"
 
 int	main(void)
 {
-	size_t	*array;
-	array = 0;
-	array = ft_calloc(100, ((size_t)1<<(sizeof(size_t) * 8 - 1)) + 1);
-	if (array)
-		ft_putendl_fd("Nao deu bom!", 1);
+	t_list	*lst = NULL;
+	char	**split = ft_split("Item1,Item2,Item3", ',');
+
+	for (int i = 0; split[i]; i++)
+		ft_lstadd_back(&lst, ft_lstnew(split[i]));
+	for(t_list *node = lst; node != NULL ; node = node->next)
+		ft_putendl_fd(node->content, 1);
+	ft_lstclear(&lst, free);
+	free(split);
 }
