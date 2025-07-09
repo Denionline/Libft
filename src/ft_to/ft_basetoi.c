3f3e5f4ccc08dbc1692078d7a52e885162f6d37e
Denionline <danielximenes164@gmail.com>
2025-07-09 11:00:33 +0100

    feat: Just align on code

--- src/ft_to/ft_basetoi.c
+++ src/ft_to/ft_basetoi.c
@@ -6,54 +6,39 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/02 17:59:45 by dximenes          #+#    #+#             */
-/*   Updated: 2025/07/09 09:59:41 by dximenes         ###   ########.fr       */
+/*   Updated: 2025/07/09 10:59:03 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int str_len(char *str)
+static int is_invalid_base(char * base)
 {
-	int	i;
+	int i;
+	int j;
 
 	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
-static int is_invalid_base(char *base)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	while (base[i] == '\0')
+	while (base[i])
 	{
 		j = i + 1;
 		while (base[j])
 		{
 			if (base[i] == base[j])
-				return (1);
+				return (TRUE);
 			j++;
 		}
-		i++;
-	}
-	i = 0;
-	while (base[i] == '\0')
-	{
 		if (base[i] == '-' || base[i] == '+')
-			return (1);
+			return (TRUE);
 		if ((base[i] >= '\a' && base[i] <= '\r') || base[i] == ' ')
-			return (1);
+			return (TRUE);
 		i++;
 	}
-	return (0);
+	return (FALSE);
 }
 
-static int find_n_in_base(char pos, char *base)
+static int find_n_in_base(char pos, char * base)
 {
-	int	i;
+	int i;
 
 	i = 0;
 	while (base[i])
@@ -65,9 +50,9 @@ static int find_n_in_base(char pos, char *base)
 	return (-1);
 }
 
-static int skip_conditions(char *str, int *m)
+static int skip_conditions(char * str, int * m)
 {
-	int	i;
+	int i;
 
 	i = 0;
 	*m = 0;
@@ -82,23 +67,25 @@ static int skip_conditions(char *str, int *m)
 	return (i);
 }
 
-int ft_basetoi(char *str, char *base)
+int ft_basetoi(char * str, char * base)
 {
-	int	base_size;
-	int	nbr_base;
-	int	result;
-	int	minus;
-	int	i;
+	int base_size;
+	int nbr_base;
+	int result;
+	int minus;
+	int i;
 
+	base_size = ft_strlen(base);
 	minus = 0;
-	if (is_invalid_base(base) || str_len(base) < 2)
+	if (is_invalid_base(base) || base_size < 2)
 		return (0);
 	i = skip_conditions(str, &minus);
-	base_size = str_len(base);
 	result = 0;
 	while (str[i])
 	{
 		nbr_base = find_n_in_base(str[i], base);
+		if (nbr_base < 0)
+			return (0);
 		result = result * base_size + nbr_base;
 		i++;
 	}
