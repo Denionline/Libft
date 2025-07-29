8f2b2b38763ffb48a1b20b7a145b817a0cdd53c8
Denionline <danielximenes164@gmail.com>
2025-07-29 10:28:17 +0100

    fix: Fixed the norm errors about the new function basetoi

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 13:21:10 by dximenes          #+#    #+#             */
-/*   Updated: 2025/07/09 10:25:08 by dximenes         ###   ########.fr       */
+/*   Updated: 2025/07/29 10:26:49 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ int		ft_isdigit(int c);
 int		ft_isascii(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
-int		ft_basetoi(char * str, char * base);
+int		ft_basetoi(char *str, char *base);
 int		ft_atoi(const char *nbr);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
--- src/ft_to/ft_basetoi.c
+++ src/ft_to/ft_basetoi.c
@@ -6,16 +6,16 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/02 17:59:45 by dximenes          #+#    #+#             */
-/*   Updated: 2025/07/09 10:59:03 by dximenes         ###   ########.fr       */
+/*   Updated: 2025/07/29 10:27:41 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int is_invalid_base(char * base)
+static int	is_invalid_base(char *base)
 {
-	int i;
-	int j;
+	int	i;
+	int	j;
 
 	i = 0;
 	while (base[i])
@@ -36,9 +36,9 @@ static int is_invalid_base(char * base)
 	return (FALSE);
 }
 
-static int find_n_in_base(char pos, char * base)
+static int	find_n_in_base(char pos, char *base)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (base[i])
@@ -50,9 +50,9 @@ static int find_n_in_base(char pos, char * base)
 	return (-1);
 }
 
-static int skip_conditions(char * str, int * m)
+static int	skip_conditions(char *str, int *m)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	*m = 0;
@@ -67,13 +67,13 @@ static int skip_conditions(char * str, int * m)
 	return (i);
 }
 
-int ft_basetoi(char * str, char * base)
+int	ft_basetoi(char *str, char *base)
 {
-	int base_size;
-	int nbr_base;
-	int result;
-	int minus;
-	int i;
+	int	base_size;
+	int	nbr_base;
+	int	result;
+	int	minus;
+	int	i;
 
 	base_size = ft_strlen(base);
 	minus = 0;
