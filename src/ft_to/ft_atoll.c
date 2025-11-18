3f829b9d1bdb7629779e9f2419e561d52253c8f1
Denionline <danielximenes164@gmail.com>
2025-11-18 16:00:32 +0000

    feat: Update

--- src/ft_to/ft_atoll.c
+++ src/ft_to/ft_atoll.c
@@ -6,24 +6,23 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 09:39:07 by dximenes          #+#    #+#             */
-/*   Updated: 2025/11/18 15:46:26 by dximenes         ###   ########.fr       */
+/*   Updated: 2025/11/18 15:59:36 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static	long long	get_num(const char *str, int i, long sign)
+static long long	get_num(const char *str, int i, long sign)
 {
-	long long	num;
-	num = 0;
+	long long num = 0;
+
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		if (num > (LLONG_MAX - (str[i] - '0')) / 10)
 		{
 			if (sign == 1)
 				return (LLONG_MAX);
-			if (sign == -1)
-				return (LLONG_MIN);
+			return (LLONG_MIN);
 		}
 		num = num * 10 + (str[i] - '0');
 		i++;
@@ -31,26 +30,20 @@ static	long long	get_num(const char *str, int i, long sign)
 	return (num * sign);
 }
 
-long long	ft_atoll(char *nptr)
+long long	ft_atoll(char *str)
 {
-	long long	num;
-	size_t		i;
-	long		sign;
-	num = 0;
-	sign = 1;
-	i = 0;
-	while (nptr[i] == ' ' || (nptr[i] >= '\t' && nptr[i] <= '\r'))
+	size_t i = 0;
+	long sign = 1;
+
+	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
 		i++;
-	if (nptr[i] == '+' || nptr[i] == '-')
+
+	if (str[i] == '+' || str[i] == '-')
 	{
-		if (nptr[i] == '-')
+		if (str[i] == '-')
 			sign = -1;
 		i++;
 	}
-	num = get_num(nptr, i, sign);
-	if (num > LLONG_MAX)
-		return (LLONG_MAX);
-	else if (num < LLONG_MIN)
-		return (LLONG_MIN);
-	return (num);
+	return get_num(str, i, sign);
 }
+
